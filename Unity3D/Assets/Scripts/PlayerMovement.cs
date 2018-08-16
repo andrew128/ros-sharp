bcc0ddd1c3c4442a0740aa474e9706dbe7cecc5a
andrew128 <andrewwei98@gmail.com>
2018-08-16 14:56:47 -0400

    player models now updating

--- Unity3D/Assets/Scripts/PlayerMovement.cs
+++ Unity3D/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    Transform headTransform;
+    Transform headRigTransform;
+    Transform leftControllerRigTransform;
+    Transform rightControllerRigTransform;
+
+    Transform headPlayerTransform;
+    Transform leftControllerPlayerTransform;
+    Transform rightControllerPlayerTransform;
+    
 
 	// Use this for initialization
 	void Start () {
-		
+        headRigTransform = GameObject.Find("Camera (eye)").transform;
+        leftControllerRigTransform = GameObject.Find("Controller (left)").transform;
+        rightControllerRigTransform = GameObject.Find("Controller (right)").transform;
+
+        headPlayerTransform = transform.Find("Head").transform;
+        leftControllerPlayerTransform = transform.Find("LeftController").transform;
+        rightControllerPlayerTransform = transform.Find("RightController").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        headPlayerTransform.position = headRigTransform.position;
+        headPlayerTransform.rotation = headRigTransform.rotation;
+        leftControllerPlayerTransform.position = leftControllerRigTransform.position;
+        leftControllerPlayerTransform.rotation = leftControllerRigTransform.rotation;
+        rightControllerPlayerTransform.position = rightControllerRigTransform.position;
+        rightControllerPlayerTransform.rotation = rightControllerRigTransform.rotation;
 	}
 }
